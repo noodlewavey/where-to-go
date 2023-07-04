f6ba77ee28fb803ad126e057a3a68edef0abf68e
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-07-04 14:54:25 -0400

    Added languages dropdown

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -1,8 +1,56 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import styled from '@emotion/styled';
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+const CustomTextField = styled(TextField)(({ theme }) => ({
+  backgroundColor: '#e4dfe0',
+  borderRadius: '2px',
+  width: '300px',
+  fontFamily: 'Work Sans',
+  marginRight:'-5px',
+  '& .MuiInputBase-input': {
+    fontFamily: 'Work Sans',
+    height: '6px',
+  },
+}));
 
 function DropdownMenu() {
     
   return (
-    <div>Dropdown menu</div>
+    <div>
+      <CustomTextField id="selectcurrency"
+      select label="Select"
+      defaultValue="ENGLISH"
+      helperText="Select your language"
+      >
+        {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+      </CustomTextField>
+    </div>
   );
 }
 
