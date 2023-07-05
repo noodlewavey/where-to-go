e35b4cd011beb60b7453375bd2c777392eafe076
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-07-05 17:25:37 -0400

    Revert "Added send button"
    This reverts commit 8570f06ae856f4fb25755fd420c2f396037339bd.

--- src/DirectionsForm.js
+++ src/DirectionsForm.js
@@ -40,75 +40,7 @@ const CustomTextField = styled(TextField)(({ theme }) => ({
     fontFamily: 'Work Sans',
     height: '6px',
   },
-<<<<<<< HEAD
-  marginRight: '3px',
-}));
-
-const DirectionsForm = ({ onLanguageSelected, onSubmit }) => {
-  const [selectedLanguageCode, setSelectedLanguageCode] = useState('');
-
-  const handleLanguageSelection = (language) => {
-    setSelectedLanguageCode(language.code);
-    onLanguageSelected(language.code);
-  };
-
-  const handleSubmit = () => {
-    onSubmit();
-  };
-
-  const languagesList = [
-    {
-      languages: [
-        { code: 'en', name: 'English' },
-        { code: 'ar', name: 'Arabic' },
-        { code: 'zh', name: 'Chinese' },
-        { code: 'fr', name: 'French' },
-        { code: 'de', name: 'German' },
-        { code: 'hi', name: 'Hindi' },
-        { code: 'id', name: 'Indonesian' },
-        { code: 'ga', name: 'Irish' },
-        { code: 'it', name: 'Italian' },
-        { code: 'ja', name: 'Japanese' },
-        { code: 'ko', name: 'Korean' },
-        { code: 'pl', name: 'Polish' },
-        { code: 'pt', name: 'Portuguese' },
-        { code: 'ru', name: 'Russian' },
-        { code: 'es', name: 'Spanish' },
-        { code: 'tr', name: 'Turkish' },
-        { code: 'vi', name: 'Vietnamese' },
-      ],
-    },
-  ];
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <CustomTextField
-        id="select-language"
-        select
-        label="Select"
-        value={selectedLanguageCode}
-        onChange={(e) => setSelectedLanguageCode(e.target.value)}
-        helperText="Select your language"
-        SelectProps={{
-          MenuProps: {
-            anchorOrigin: {
-              vertical: 'bottom',
-              horizontal: 'center',
-            },
-            getContentAnchorEl: null,
-          },
-        }}
-      >
-        {languagesList[0].languages.map((language) => (
-          <MenuItem key={language.code} value={language.code}>
-            {language.name}
-          </MenuItem>
-        ))}
-      </CustomTextField>
-      <Button variant="outlined" endIcon={<SendIcon />} onClick={handleSubmit}>
-        Send
-      </Button>
-=======
+  marginRight:'3px',
 }));
 
 export default function DirectionsForm() {
