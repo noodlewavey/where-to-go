cf242ea36311cbe67dd36150e0f57b2a6cef5e66
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-07-05 17:24:21 -0400

    fucked up aagain

--- src/DirectionsForm.js
+++ src/DirectionsForm.js
@@ -1,31 +1,10 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import AddLocationIcon from '@mui/icons-material/AddLocation';
-import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
-import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import DropdownMenu from './DropdownMenu';
-import { TextField } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
-import Stack from '@mui/material/Stack';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#e4dfe0',
-  padding: theme.spacing(1),
-  color: theme.palette.text.secondary,
-  fontFamily: 'Work Sans',
-  border: '1px solid #000',
-  display: 'flex',
-  alignItems: 'center',
-  '& > *': {
-    margin: theme.spacing(1),
-  },
-}));
 
 const CustomTextField = styled(TextField)(({ theme }) => ({
   backgroundColor: '#e4dfe0',
@@ -36,47 +15,75 @@ const CustomTextField = styled(TextField)(({ theme }) => ({
     fontFamily: 'Work Sans',
     height: '6px',
   },
-  marginRight:'3px',
+  marginRight: '3px',
 }));
 
-export default function DirectionsForm() {
+const DirectionsForm = ({ onLanguageSelected, onSubmit }) => {
+  const [selectedLanguageCode, setSelectedLanguageCode] = useState('');
+
+  const handleLanguageSelection = (language) => {
+    setSelectedLanguageCode(language.code);
+    onLanguageSelected(language.code);
+  };
+
+  const handleSubmit = () => {
+    onSubmit();
+  };
+
+  const languagesList = [
+    {
+      languages: [
+        { code: 'en', name: 'English' },
+        { code: 'ar', name: 'Arabic' },
+        { code: 'zh', name: 'Chinese' },
+        { code: 'fr', name: 'French' },
+        { code: 'de', name: 'German' },
+        { code: 'hi', name: 'Hindi' },
+        { code: 'id', name: 'Indonesian' },
+        { code: 'ga', name: 'Irish' },
+        { code: 'it', name: 'Italian' },
+        { code: 'ja', name: 'Japanese' },
+        { code: 'ko', name: 'Korean' },
+        { code: 'pl', name: 'Polish' },
+        { code: 'pt', name: 'Portuguese' },
+        { code: 'ru', name: 'Russian' },
+        { code: 'es', name: 'Spanish' },
+        { code: 'tr', name: 'Turkish' },
+        { code: 'vi', name: 'Vietnamese' },
+      ],
+    },
+  ];
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1} justifyContent="center">
-        <Grid item xs={11.6} sx={{ marginTop: '12px' }}>
-          <Item>
-            <AddLocationIcon />
-            <strong style={{ marginRight: 'auto' }}>Origin:</strong>
-            <CustomTextField id="Origin" />
-          </Item>
-        </Grid>
-        <Grid item xs={11.6}>
-          <Item>
-            <AddLocationAltIcon />
-            <strong style={{ marginRight: 'auto' }}>Destination:</strong>
-            <CustomTextField id="Destination" />
-          </Item>
-        </Grid>
-        <Grid item xs={11.6}>
-          <Item>
-            <AirportShuttleIcon />
-            <strong style={{ marginRight: 'auto' }}>Transport Mode:</strong>
-            <CustomTextField id="Mode" />
-          </Item>
-        </Grid>
-        <Grid item xs={11.6} sx={{ marginBottom: '12px' }}>
-          <Item>
-            <ChatBubbleOutlineIcon />
-            <strong style={{ marginLeft: '-7px' }}>Target Lang:</strong>
-            <DropdownMenu />
-            <Button variant="outlined" endIcon={<SendIcon />} sx={{ color: 'black', height:'57px', marginTop:'-21px'}}>
-              Send
-            </Button>
-          </Item>
-        </Grid>
-      </Grid>
+      <CustomTextField
+        id="select-language"
+        select
+        label="Select"
+        value={selectedLanguageCode}
+        onChange={(e) => setSelectedLanguageCode(e.target.value)}
+        helperText="Select your language"
+        SelectProps={{
+          MenuProps: {
+            anchorOrigin: {
+              vertical: 'bottom',
+              horizontal: 'center',
+            },
+            getContentAnchorEl: null,
+          },
+        }}
+      >
+        {languagesList[0].languages.map((language) => (
+          <MenuItem key={language.code} value={language.code}>
+            {language.name}
+          </MenuItem>
+        ))}
+      </CustomTextField>
+      <Button variant="outlined" endIcon={<SendIcon />} onClick={handleSubmit}>
+        Send
+      </Button>
     </Box>
   );
-}
+};
 
-//margin-right:auto with flex-direction:row flex container...pushes item to rightmost side of container
+export default DirectionsForm;
